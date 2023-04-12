84ef717532e3ced289528cbf6902182c22002e2a
SangCheol <scpark0698@gmail.com>
2023-04-12 19:15:28 +0900

    Add Employee Controller

---
+++ src/main/java/cuk/resttutorials/payroll/EmployeeController.java
@@ -0,0 +1,51 @@
+package cuk.resttutorials.payroll;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee save(@RequestBody Employee employee) {
+        return repository.save(employee);
+    }
+
+    //Single item
+
+    @GetMapping("/employees/{id}")
+    Employee one(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newEmployee.setId(id);
+                    return repository.save(newEmployee);
+                });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/cuk/resttutorials/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package cuk.resttutorials.payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    public EmployeeNotFoundException(Long id) {
+        super("Could not find Employee " + id);
+    }
+}
